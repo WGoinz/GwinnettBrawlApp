723c118f3b3b37335e18d1c9724dab79b83e029c
Walter Goins <wgoinz@gmail.com>
2019-02-21 09:15:52 -0500

    registered models on admin.py

--- backend/gbrawl/admin.py
+++ backend/gbrawl/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Tournament, Event, Participant
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Tournament)
+admin.site.register(Event)
+admin.site.register(Participant)
