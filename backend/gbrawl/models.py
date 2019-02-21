22304b570909ab73764e2aa01d1dee13b4b3ee61
Walter Goins <wgoinz@gmail.com>
2019-02-21 08:57:57 -0500

    added user model

--- backend/gbrawl/models.py
+++ backend/gbrawl/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=120)
+    email = models.CharField(max_length=120)
+    password = models.CharField(max_length=120)
+    slogan = models.CharField(max_length=120)
+    profilePic = models.TextField()
+
+    def _str_(self):
+        return self.username
