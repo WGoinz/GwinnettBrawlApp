15d3862bca0beb3f8c44bbdcd6625c754a25ebf3
Walter Goins <wgoinz@gmail.com>
2019-02-21 09:42:47 -0500

    added user view

--- backend/gbrawl/views.py
+++ backend/gbrawl/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import UserSerializer, TournamentSerializer, EventSerializer, ParticipantSerializer
+from .models import User, Tournament, Event, Participant
 
-# Create your views here.
+
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
