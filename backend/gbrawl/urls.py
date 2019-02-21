ade14c4e1b7386a8e0ccf6f920a8edd41c141ad9
Walter Goins <wgoinz@gmail.com>
2019-02-21 10:31:03 -0500

    nested second level events route with drf-nested-routers

--- backend/gbrawl/urls.py
+++ backend/gbrawl/urls.py
@@ -6,9 +6,14 @@ from rest_framework_nested import routers
 router = routers.DefaultRouter()
 router.register(r'users', views.UserView)
 
-domains_router = routers.NestedSimpleRouter(router, r'users', lookup='user')
-domains_router.register(
+user_router = routers.NestedSimpleRouter(router, r'users', lookup='user')
+user_router.register(
     r'tournaments', views.TournamentView, base_name='user-tournaments')
+
+tournament_router = routers.NestedSimpleRouter(
+    user_router, r'tournaments', lookup='tournament')
+tournament_router.register(
+    r'events', views.EventView, base_name='tournament-events')
 
 # router.register("tournaments", views.TournamentView)
 # router.register('events', views.EventView)
@@ -17,5 +22,6 @@ domains_router.register(
 
 urlpatterns = [
     path(r'', include(router.urls)),
-    path(r'', include(domains_router.urls)),
+    path(r'', include(user_router.urls)),
+    path(r'', include(tournament_router.urls)),
 ]
