371c369080bbe6d75d1614bf13d819646e35f0da
Walter Goins <wgoinz@gmail.com>
2019-02-25 11:00:34 -0500

    Setup starting info for adding user authentication using jwt token

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ https://trello.com/b/c7te4cgt/gwinnettbrawlapp
 ![ERD](/public/images/GwinnettBrawlApp_ERD.png)
 ![Gwinnett Brawl App WireFrame](/public/images/GwinnettBrawlApp_Wireframe.png)
 * May differ from final product
+
+https://github.com/alanjds/drf-nested-routers
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -121,6 +122,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
 CORS_ORIGIN_WHITELIST = (
     'localhost:3000/'
 )
