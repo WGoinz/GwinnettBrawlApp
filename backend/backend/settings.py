ce92b25edd3df09367d8052750c23e00429b877f
Walter Goins <wgoinz@gmail.com>
2019-02-28 16:42:21 -0500

    setup postgres database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'gbrawl',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
@@ -78,8 +79,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gbrawl',
+        'USER': 'sample',
+        'PASSWORD': 'sample',
+        'HOST': 'localhost'
     }
 }
 
@@ -125,4 +129,3 @@ STATIC_URL = '/static/'
 CORS_ORIGIN_WHITELIST = (
     'localhost:3000/'
 )
-
